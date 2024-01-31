94e36f89f2fcd693ab9153faf6c6d797bc87ddbb
Agustin Riondini <105334088+agustinriondini@users.noreply.github.com>
2024-01-31 00:10:34 -0300

    Initial_release_content

---
+++ Resources/kernel/animations.js
@@ -0,0 +1,27 @@
+/*commandcenter*/
+document.addEventListener("DOMContentLoaded", function() {
+    var logo = document.querySelector(".logoContainer");
+    var nav = document.querySelector(".navContainer");
+    var links = document.querySelectorAll(".navContainer nav ul li a");
+    setTimeout(function() {
+        logo.classList.add("aparecer");
+    }, 100);
+    setTimeout(function() {
+        nav.classList.add("aparecer");
+    }, 300);
+    links.forEach(function(link, index) {
+        setTimeout(function() {
+        link.classList.add("aparecer");
+      }, 500 + index * 100);
+    });
+});
+/*profile*/
+document.addEventListener("DOMContentLoaded", function() {
+    var profile = document.getElementById("profile");
+    profile.classList.add("loaded");
+});
+/*projects*/
+document.addEventListener("DOMContentLoaded", function() {
+    var projects = document.getElementById("projects");
+    projects.classList.add("loaded");
+});
