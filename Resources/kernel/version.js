49a6440f43428e328d6b4c217fc204d6eff2f558
Agustin Riondini <105334088+agustinriondini@users.noreply.github.com>
2024-11-01 01:40:40 +0000

    pic-hotfix-mobile#build16

--- Resources/kernel/version.js
+++ Resources/kernel/version.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var actualver = $('#updateVer').text();
-    var lastUpdate = "public_release_v1.2.1_update_31102024_build#15";
+    var lastUpdate = "public_release_v1.2.1_update_31102024_build#16";
 
     console.log('Valor actual:', actualver);
 
